c5fb9704007e53add2edb796a51a7172db1f9c3f
elenahao <elena_haoo@163.com>
2019-12-09 18:02:21 +0800

    main

--- src/view/app.jsx
+++ src/view/app.jsx
@@ -1,18 +1,12 @@
 import React from "react";
-import { Button } from "antd";
+import Main from '../component/Main.jsx';
 import "../static/index.css";
 
-export default class App extends React.Component {
+
+class App extends React.Component {
     render() {
-        return (
-            <div className="App">
-                <h1>按钮展示：</h1>
-                <Button type="primary">Primary</Button>
-                <Button>Default</Button>
-                <Button type="dashed">Dashed</Button>
-                <Button type="danger">Danger</Button>
-                <Button type="link">Link</Button>
-            </div>
-        );
+        <Main/>
     }
 }
+
+export default App;
